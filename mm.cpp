f3a9a255e4988b9c51f3dc7b74bfdd962344c82c
Prashanth Soundarapandian <31555784+prashanthsoundar@users.noreply.github.com>
2018-04-27 23:55:35 -0400

    Update mm.cpp

--- mm.cpp
+++ mm.cpp
@@ -5,17 +5,27 @@
 #include<unordered_map>
 #include <vector>
 #include <string.h>
+#include <algorithm>
 #include <mutex>
 #include <thread>
 #include <chrono>
 
-void setVMCurrentMemoryUsage(std::pair<std::string,double>& vm);
+struct dataValues
+{
+    int currentMemory;
+    int maxPeak;
+};
+
+void setVMCurrentMemoryUsage(std::pair<std::string,struct dataValues*>& vm);
 
 std::vector<std::thread> monitorThreads;
 //swpd   free   buff  cache
 // 2       3      4     5
 std::vector<int> hostStats;
-static std::vector<std::pair<std::string,double>> vm;
+static std::vector<std::pair<std::string,struct dataValues*>> vm;
+static std::vector<int> max_peak;
+
+
 
 std::string runCommand(const char *s)
 {
@@ -36,21 +46,27 @@ void startMonitoring()
     for(int i=0;i<vm.size();i++) {
 
         monitorThreads.push_back(std::thread([=] {
-
+            int timer  = 1, maxMem =0;
             while(1) {
                 std::cout<<vm[i].first<<std::endl;
                 setVMCurrentMemoryUsage(vm[i]);
                 std::this_thread::sleep_for (std::chrono::seconds(1));
-                std::cout<<vm[i].second<<" ";
+                std::cout<<vm[i].second->currentMemory<<" ";
+                maxMem = std::max(vm[i].second-> currentMemory,maxMem);
+                timer++;
+                if(timer==10)
+                {
+                    timer=0;
+                    vm[i].second->maxPeak = maxMem;
+                    std::cout<<"30-------------"<<vm[i].second->maxPeak<<std::endl;
+                    maxMem=0;
+                }
             }
         }));
 
     }
 
-    for(auto& t : monitorThreads)
-    {
-        t.join();
-    }
+
 
 }
 
@@ -78,7 +94,9 @@ void  MemoryMonitor() {
             {
                 if(j%2==1)
                 {
-                    vm.push_back(make_pair(to,0));
+                    struct dataValues* dv = new struct dataValues();
+                    dv->currentMemory=dv->maxPeak=0;
+                    vm.push_back(make_pair(to,dv));
                 }
                 j++;
             }
@@ -90,7 +108,7 @@ void  MemoryMonitor() {
 
 }
 
-void setVMCurrentMemoryUsage(std::pair<std::string,double>& vm)
+void setVMCurrentMemoryUsage(std::pair<std::string,struct dataValues*>& vm)
 {
      std::string data = runCommand(("/usr/bin/virsh dommemstat " + vm.first).c_str());
 
@@ -99,11 +117,36 @@ void setVMCurrentMemoryUsage(std::pair<std::string,double>& vm)
      std::smatch s;
 
      regex_search(data,s,r1);
-     vm.second = stod(s.str(1));
+     vm.second->currentMemory = stod(s.str(1));
 
      regex_search(data,s,r2);
 
-     vm.second = (100 - (stod(s.str(1))/vm.second)*100);
+    vm.second->currentMemory -= stod(s.str(1));
+}
+
+void startProcessing()
+{
+
+    monitorThreads.push_back(std::thread([=] {
+
+        int timer  = 1;
+
+        while(1)
+        {
+            if (timer == 30)
+            {
+                for(int i=0;i<vm.size();i++)
+                {
+                    std::cout <<"30 Sec Read: "<<vm[i].second->maxPeak << std::endl;
+                }
+                timer=0;
+            }
+            std::this_thread::sleep_for (std::chrono::seconds(1));
+            timer+=1;
+        }
+    }));
+
+
 }
 
 void setHostCurrentMemoryUsage()
@@ -138,8 +181,7 @@ void setHostCurrentMemoryUsage()
     std::cout<<hostStats[4]<<" ";
     std::cout<<hostStats[5]<<" ";
 
-//        std::cout<<hostStats[2]+hostStats[3]+hostStats[4]+hostStats[5]<<" ";
-//
+
 }
 
 
@@ -160,6 +202,13 @@ int main(int argc,char* argv[]) {
 
     MemoryMonitor();
     startMonitoring();
+    std::this_thread::sleep_for (std::chrono::seconds(5));
+    startProcessing();
+
+    for(auto& t : monitorThreads)
+    {
+        t.join();
+    }
 
     return 0;
 }
