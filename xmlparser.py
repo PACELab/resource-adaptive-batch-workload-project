419d0342df4371659a44d2376b8b7f200ed13c10
Prashanth Soundarapandian <31555784+prashanthsoundar@users.noreply.github.com>
2018-05-17 19:12:14 -0400

    Update xmlparser.py

--- xmlparser.py
+++ xmlparser.py
@@ -1,14 +1,12 @@
 import xml.etree.ElementTree as ET
 import sys
 
-tree = ET.parse(sys.argv[3])
+tree = ET.parse(sys.argv[1])
 
 
 for child in tree.getroot():
     prop = child.find('name')
     if(prop.text == 'yarn.scheduler.maximum-allocation-mb'):
-        prop.value = int(sys.argv[1])
-    elif(prop.text == 'yarn.scheduler.minimum-allocation-mb'):
         prop.value = int(sys.argv[2])
 
 tree.write(sys.argv[3])
