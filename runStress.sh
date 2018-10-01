db9baeac2ebd753805d5e0d2e2cc6478c8d14159
Prashanth Soundarapandian <31555784+prashanthsoundar@users.noreply.github.com>
2018-10-01 00:16:07 -0400

    Update runStress.sh

--- runStress.sh
+++ runStress.sh
@@ -1,2 +1,2 @@
-docker exec -d --user test $1 /usr/bin/stress-ng --vm 32 --vm-bytes ${2}K
-echo "docker exec -d --user test $1 /usr/bin/stress-ng --vm 32 --vm-bytes ${2}K"
+docker exec -d --user test $1 /usr/bin/stress-ng --vm $2 --vm-bytes ${3}K
+echo "docker exec -d --user test $1 /usr/bin/stress-ng --vm $2 --vm-bytes ${3}K"
