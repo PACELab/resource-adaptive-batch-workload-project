da0c3d7ab30622c7262a390a35a237d5b44d4e8b
Amoghavarsha <amoghavs@gmail.com>
2018-12-18 18:13:40 -0500

    Network control is mostly OK, but not sure why FG bw keeps fluctuating. Should test the logic properly.

--- network_control/parse_traces.py
+++ network_control/parse_traces.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+import sys,commands,datetime
 """ This file parses the raw tracepktdump output to outputs
 the sum of ingress + outgress traffic along with the seconds"""
 
@@ -5,17 +7,21 @@ the sum of ingress + outgress traffic along with the seconds"""
 
 # Dump the tracepktdump output to a file and
 # use that as an input to this script
-file_name = "/data_repo/dl_wits_data/traces.txt"
+file_name = "trace_files/tracesMay.txt"
 
 # TODO: change this value according to the bundle you use
-kwd = 'Oct 20'
+kwd = 'May 12'
+pkwd = 'May12'
 
 res = {}
 packet_cnt = {}
+lineCnt = 0
+prevTime = datetime.datetime.now()
 with open(file_name) as fp:
     for line in fp:
         try:
             if kwd in line:
+                #print "\t line: %s "%(line)
                 time = line.split(' ')[3]
                 hour, min, sec = list(map(int, time.split(":")))
                 key = hour * 3600 + min * 60 + sec
@@ -23,11 +29,13 @@ with open(file_name) as fp:
                     packet_cnt[key] += 1
                 else:
                     packet_cnt[key] = 1
+                #print "\t *** key: %s *** "%(key)
             if 'Packet Length:' in line:
                 val = int(line.split(' ')[4].split('/')[1])
 
             if 'Direction Value:' in line:
                 index = int(line.split(' ')[-1])
+
 
                 if key in res:
                     res[key][index] += val
@@ -35,15 +43,30 @@ with open(file_name) as fp:
                     res[key] = [0, 0, 0]
                     res[key][index] += val
         except Exception as e:
+            #continue;
             print line, e
-
-print res
+        
+        lineCnt+=1
+        if(lineCnt%(1000*1000)==0): 
+            currTime = datetime.datetime.now()
+            delta = currTime - prevTime
+            print "\t line: %d M time: %s "%((lineCnt/1e6),delta)
+            #break;
+        #if(lineCnt>100*1000): break;
+#print res
 # print 'Packets/sec: ', packet_cnt
+
 
 _keys = res.keys()
 _keys.sort()
 
 # default output of this script goes to this file
-with open('trace_result.txt', 'w') as fp:
-    for key in _keys:
-        fp.write('{} \n'.format((res[key][0] + res[key][1]) * 11.8))
+fp1 = open('trace_result_1_'+str(pkwd)+'.txt', 'w')
+fp2 = open('trace_result_2_'+str(pkwd)+'.txt', 'w')
+for key in _keys:
+    #fp.write('{} \n'.format((res[key][0] + res[key][1]) * 11.8))
+    l1 = '%s\n'%(res[key][0]*11.8)
+    l2 = '%s\n'%(res[key][1]*11.8)
+    fp1.write(l1)
+    fp2.write(l2)
+
--- network_control/simulate.py
+++ network_control/simulate.py
@@ -1,4 +1,6 @@
-import subprocess
+#! /usr/bin/python
+
+import sys,subprocess,commands
 
 orig_container_bw = raw_input('Input Original Container bandwidth (do not specify units): ')
 phase_change_samples = raw_input('Input values for phase change samples (comma separated): ').strip().split(',')
@@ -10,10 +12,21 @@ reduce_container_bw = raw_input('Input the value by which you want to reduce con
 # Now manipulate the cpp script to take this file as input and compile the file to rename
 # the object file as network-control.out
 
+# Plan: 
+# 1. Parameters to sweep
+#      std-dev threshold; phase-change-samples; traces
+# 2. How to sweep
+#       traces --> std-dev-threshold --> phase-change-samples.
+# 3. Metrics to collect
+#   Violations, #Phase changes, FG BW, BG BW, 
+# 4. How to summarize
+#   per trace, 
+
 flag = True
 for phase in phase_change_samples:
     for stdev in std_dev_threshold:
         res = subprocess.check_output(['./network-control.out', '1', '{}'.format(orig_container_bw),
                                        '{}'.format(phase), '0.0', '{}'.format(stdev),
                                        '{}'.format(reduce_container_bw), 'netstat'])
-        # print res
+
+
